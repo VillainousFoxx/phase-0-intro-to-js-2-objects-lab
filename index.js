d764a8352e70c7ae41d13e9344dc3956baf5f684
Abby <awurster821@gmail.com>
2023-10-02 18:39:45 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Write your solution in this file//
+
+const employee = {
+    name: "vfox",
+    streetAddress: "123 Broad",
+};
+
+ function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+
+    newEmployee.name = "Sam";
+    newEmployee.streetAddress = "11 Broadway";
+
+    return newEmployee;
+
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress,value){
+    employee.streetAddress = "12 Broadway";
+    return employee
+ }
+
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+
+    return newEmployee;
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+     delete employee.name;
+     return employee;
+}
+
